164d283764c7d6bd28f0f6ef8c75d8ffd8c89e57
SpanishBigHam <penghack@foxmail.com>
2018-03-05 15:27:07 +0800

    create project

---
+++ NetStatusSharp/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace NetStatusSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(832, 342);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 30;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "进程";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "协议";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 60;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "本地地址";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 120;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "端口";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 70;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "远程地址";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 120;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "端口";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 70;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "状态";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "进程/PID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "本地端口";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(251, 390);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "远程端口";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(327, 390);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "全部",
+            "TCP",
+            "UDP"});
+            this.comboBox1.Location = new System.Drawing.Point(399, 390);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(63, 20);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "协议";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 422);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+    }
+}
+
---
+++ NetStatusSharp/Form1.cs
@@ -0,0 +1,166 @@
+﻿using ProcessViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetStatusSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.Rows.Clear();
+            //tcp
+            if (this.comboBox1.SelectedText == "" || this.comboBox1.SelectedText == "全部" || this.comboBox1.SelectedText == "TCP")
+            {
+                var processNames = new List<string>();
+                var allConns = NetProcessAPI.GetAllTcpConnections();
+                //pid
+                if (!string.IsNullOrEmpty(this.textBox1.Text))
+                {
+                    var t = this.textBox1.Text.Split(',');
+                    foreach (var p in t)
+                    {
+                        if (Int64.TryParse(p.Trim(), out long pid))
+                        {
+                            allConns = allConns.Where(x => x.owningPid == pid).ToArray();
+                        }
+                        else
+                        {
+                            processNames.Add(p.Trim());
+                        }
+                    }
+
+                }
+                //local port
+                if (!string.IsNullOrEmpty(this.textBox2.Text))
+                {
+                    var t = this.textBox2.Text.Split(',');
+                    foreach (var p in t)
+                    {
+                        if (Int32.TryParse(p.Trim(), out int port))
+                        {
+                            allConns = allConns.Where(x => x.LocalPort == port).ToArray();
+                        }
+                    }
+
+                }
+                //remote port
+                if (!string.IsNullOrEmpty(this.textBox3.Text))
+                {
+                    var t = this.textBox3.Text.Split(',');
+                    foreach (var p in t)
+                    {
+                        if (Int32.TryParse(p.Trim(), out int port))
+                        {
+                            allConns = allConns.Where(x => x.RemotePort == port).ToArray();
+                        }
+                    }
+                }
+                if (allConns.Any())
+                {
+                    foreach (var p in allConns)
+                    {
+
+                        var pname = ProcessAPI.GetProcessNameByPID(p.owningPid);
+                        if (!processNames.Any() || processNames.Any(x => x.ToLower().Contains(pname.ToLower())))
+                        {
+                            var icon = ProcessAPI.GetIcon(p.owningPid, true);
+                            dataGridView1.Rows.Add(new object[]
+                            {
+                                icon,
+                                pname+" "+p.owningPid,
+                                "TCP",
+                                p.LocalAddress.ToString(),
+                                p.LocalPort.ToString(),
+                                p.RemoteAddress.ToString(),
+                                p.RemotePort.ToString(),
+                                p.state.ToString()
+                             });
+                        }
+
+                    }
+                }
+            }
+
+
+            //udp
+            if (this.comboBox1.SelectedText == "" || this.comboBox1.SelectedText == "全部" || this.comboBox1.SelectedText == "UDP")
+            {
+                var allUconns = NetProcessAPI.GetAllUdpConnections();
+                var processNames = new List<string>();
+                //pid
+                if (!string.IsNullOrEmpty(this.textBox1.Text))
+                {
+                    var t = this.textBox1.Text.Split(',');
+                    foreach (var p in t)
+                    {
+                        if (Int64.TryParse(p.Trim(), out long pid))
+                        {
+                            allUconns = allUconns.Where(x => x.owningPid == pid).ToArray();
+                        }
+                        else
+                        {
+                            processNames.Add(p.Trim());
+                        }
+                    }
+                }
+                //local port
+                if (!string.IsNullOrEmpty(this.textBox2.Text))
+                {
+                    var t = this.textBox2.Text.Split(',');
+                    foreach (var p in t)
+                    {
+                        if (Int32.TryParse(p.Trim(), out int port))
+                        {
+                            allUconns = allUconns.Where(x => x.LocalPort == port).ToArray();
+                        }
+                    }
+                }
+                if (allUconns != null)
+                {
+                    foreach (var p in allUconns)
+                    {
+                        var pname = ProcessAPI.GetProcessNameByPID(p.owningPid);
+                        if (!processNames.Any() || processNames.Any(x => pname.ToLower().Contains(x.ToLower())))
+                        {
+                            var icon = ProcessAPI.GetIcon(p.owningPid, true);
+                            dataGridView1.Rows.Add(new object[]
+                            {
+                                    icon,
+                                    pname+" "+p.owningPid,
+                                    "UDP",
+                                    p.LocalAddress.ToString(),
+                                    p.LocalPort.ToString(),
+                                    "-",
+                                    "-",
+                                    "-"
+                            });
+                        }
+
+                    }
+                }
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
